fe70e08ea5031e635a4af1e76955f48a0c9a324b
elisabethedert <elisabeth.edert@gmail.com>
2023-11-25 20:53:26 +0100

    scenery and first attempts of objects

--- public/snake/src/main.js
+++ public/snake/src/main.js
@@ -1,12 +1,236 @@
-import kaboom from "kaboom"
+import kaboom from "kaboom";
 
-const k = kaboom()
+const WIDTH = 850;
+const HEIGHT = 600;
+const FIELD = 25;
+const BRICK_HEIGHT = 50;
+var speed = 80;
 
-k.loadSprite("bean", "sprites/bean.png")
+// initialize context
+kaboom({ width: WIDTH, height: HEIGHT });
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
-])
+// load assets
+loadSprite("snake", "sprites/bean.png");
+setBackground([0, 120, 0]);
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+//Game Sceme
+scene("game", () => {
+  // bricks
+  // bottom
+  add([
+    rect(WIDTH, BRICK_HEIGHT),
+    pos(0, HEIGHT),
+    anchor("botleft"),
+    area(),
+    body({ isStatic: true }),
+    color(156, 72, 44),
+    "brick-b",
+  ]);
+
+  // top
+  add([
+    rect(WIDTH, BRICK_HEIGHT),
+    pos(0, 0),
+    area(),
+    body({ isStatic: true }),
+    color(156, 72, 44),
+    "brick-t",
+  ]);
+
+  // left
+  add([
+    rect(BRICK_HEIGHT, WIDTH),
+    pos(0, 0),
+    area(),
+    body({ isStatic: true }),
+    color(156, 72, 44),
+    "brick-l",
+  ]);
+
+  // right
+  add([
+    rect(BRICK_HEIGHT, WIDTH),
+    pos(WIDTH - BRICK_HEIGHT, 0),
+    area(),
+    body({ isStatic: true }),
+    color(156, 72, 44),
+    "brick-r",
+  ]);
+
+  // keep track of score
+  let score = 0;
+  const scoreLabel = add([
+    text(score),
+    pos(BRICK_HEIGHT / 2, BRICK_HEIGHT / 2),
+  ]);
+
+  // add player object to screen
+  var player = add([
+    // list of components
+    sprite("snake"),
+    // rect(FIELD, FIELD),
+    pos(WIDTH - 400, HEIGHT / 2 - 25),
+    area(),
+    // color(255, 20, 20),
+    // move(RIGHT, speed),
+    "snake",
+  ]);
+
+  function addTail() {
+    //TODO
+  }
+
+  //MOVEMENT TESTS
+  //   const movementR = onUpdate("snake", (player) => {
+  //     player.move(speed, 0);
+  // })
+  //   const movementL = onUpdate("snake", (player) => {
+  //     player.move(-speed, 0);
+  // })
+  //   const movementU = onUpdate("snake", (player) => {
+  //     player.move(1, -speed);
+  // })
+  //   const movementD = onUpdate("snake", (player) => {
+  //     player.move(0, speed);
+  // })
+
+  // moves snake by key-press
+  onKeyDown("right", () => {
+    player.move(speed, 0);
+    // movementR();
+  });
+  onKeyDown("left", () => {
+    player.move(-speed, 0);
+    // movementL();
+  });
+  onKeyDown("up", () => {
+    player.move(0, -speed);
+    // movementU();
+  });
+  onKeyDown("down", () => {
+    player.move(0, speed);
+    // movementD();
+  });
+
+  // add fruits to game
+  function showFruit() {
+    add([
+      rect(FIELD, FIELD),
+      area(),
+      // outline(4),
+      pos(
+        rand(BRICK_HEIGHT, WIDTH - BRICK_HEIGHT - FIELD),
+        rand(BRICK_HEIGHT + FIELD, HEIGHT - BRICK_HEIGHT)
+      ),
+
+      anchor("botleft"),
+      color(255, 180, 255),
+      // move(LEFT, speed),
+      "fruit",
+    ]);
+
+    // wait a random amount of time to spawn next tree
+    // wait(rand(0.5, 1.5), spawnTree);
+  }
+
+  showFruit();
+
+  // add obstacles
+  function showMole() {
+    add([
+      rect(FIELD, FIELD),
+      area(),
+      pos(
+        rand(BRICK_HEIGHT, WIDTH - BRICK_HEIGHT - FIELD),
+        rand(BRICK_HEIGHT + FIELD, HEIGHT - BRICK_HEIGHT)
+      ),
+      anchor("botleft"),
+      color(87, 50, 3),
+      "mole",
+    ]);
+  }
+
+  function showBush() {
+    add([
+      rect(rand(FIELD, 3 * FIELD), rand(FIELD, 3 * FIELD)),
+      area(),
+      pos(
+        rand(BRICK_HEIGHT, WIDTH - BRICK_HEIGHT - FIELD),
+        rand(BRICK_HEIGHT + FIELD, HEIGHT - BRICK_HEIGHT)
+      ),
+      anchor("botleft"),
+      color(39, 125, 60),
+      "bush",
+    ]);
+  }
+
+  // lose if player collides with any game obj except for fruits
+  player.onCollide("brick-l", () => {
+    // go to "lose" scene and pass the score
+    go("lose", score);
+    // burp(); //sound
+  });
+  player.onCollide("brick-r", () => {
+    go("lose", score);
+  });
+  player.onCollide("brick-t", () => {
+    go("lose", score);
+  });
+  player.onCollide("brick-b", () => {
+    go("lose", score);
+  });
+  player.onCollide("mole", () => {
+    go("lose", score);
+  });
+  player.onCollide("bush", () => {
+    go("lose", score);
+  });
+
+  player.onCollide("fruit", (fruit) => {
+    score++;
+    scoreLabel.text = score;
+    destroy(fruit);
+    addCollisionObjectandSpeed();
+    showFruit();
+  });
+
+  function addCollisionObjectandSpeed() {
+    if (score === 3 || score === 4 || score === 6) {
+      speed = speed + 30;
+      showMole();
+    } else if (score === 4 || score === 6 || score === 10) {
+      speed = speed + 40;
+      showBush();
+    }
+  }
+});
+
+// EndScene with Highscore
+scene("lose", (score) => {
+  add([
+    sprite("snake"),
+    pos(WIDTH / 2, HEIGHT / 2 - 80),
+    scale(2),
+    anchor("center"),
+  ]);
+
+  // display score
+  add([
+    text(score),
+    pos(WIDTH / 2, HEIGHT / 2 + 80),
+    scale(2),
+    anchor("center"),
+  ]);
+
+  add([
+    text("Play again? Press Space-Key"),
+    pos(WIDTH / 2, HEIGHT / 2 + 140),
+    scale(0.75),
+    anchor("center"),
+  ]);
+
+  // go back to game with space is pressed
+  onKeyPress("space", () => go("game"));
+});
+
+go("game");
