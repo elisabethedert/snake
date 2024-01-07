bb8577e814cfd6060db580df3b4d3e8991d134c7
elisabethedert <elisabeth.edert@gmail.com>
2024-01-07 21:00:36 +0100

    pause collision with obstacles by boolean

--- public/snake/src/funktionalities/collision.js
+++ public/snake/src/funktionalities/collision.js
@@ -10,8 +10,7 @@ export default class Collsion {
     this.speed = speed;
     this.scoreLabel = null;
     this.score = 0;
-    this.collisionArr = [];
-    this.lastElement;
+    this.isSupersnake = false;
   }
 
   showScoreLabel() {
@@ -26,48 +25,35 @@ export default class Collsion {
   }
 
   collide() {
-    let collisionfunktion;
-    
-    // wird angelegt, damit man bei Spielstart nicht durch Wände gehen kann
-    if (this.collisionArr.length == 0) {
-      console.log(this.collisionArr.length);
-      collisionfunktion = this.collisionWithObstacles();
-    }
+    k.onCollide("snake", "superfruit", (s, f) => {
+      this.isSupersnake = true;
+      this.sprites.showFruit();
+      this.showScoreLabel();
+      // setTimeout(function () {
+      //   this.isSupersnake = false;
+      //   console.log(this.isSupersnake);
+      //   console.log("snake ist wieder false");
+      // }, 3000);
+    });
 
-    k.onCollide("snake", "fruit", (s, f) => {
-      // snake_length++;
+    k.onCollide("snake", "fruit", (s, sf) => {
+      this.isSupersnake = false;
       this.score++;
       this.addCollisionObjectandSpeed();
+      // snake_length++;
       this.sprites.showFruit();
       this.showScoreLabel();
-      this.collisionArr.push("fruit");
-      this.lastElement = this.collisionArr[this.collisionArr.length - 1];
-      if (this.lastElement === "fruit") {
-        k.wait(5, function () {
-          console.log("frucht gegessen, Specialeffekt vorbei");
-          // auf console log wird gewartet
-          // auf Funktion wird nicht gewartet und man kann weiterhin mit brick / obstacle collidieren
-          collisionfunktion;
-        });
-      }
     });
 
-    k.onCollide("snake", "superfruit", (s, sf) => {
-      this.sprites.showFruit();
-      this.showScoreLabel();
-      this.collisionArr.push("superfruit");
-      this.lastElement = this.collisionArr[this.collisionArr.length - 1];
-      console.log(this.lastElement);
-    });
-  }
-
-  collisionWithObstacles() {
-    console.log("Funktion wird aufgerufen");
     k.onCollide("snake", "brick", (s, b) => {
+      console.log("Collision Brick " + this.isSupersnake);
       k.go("Lose", this.score);
     });
+
     k.onCollide("snake", "obstacle", (s, m) => {
-      k.go("Lose", this.score);
+      if (this.isSupersnake == false) {
+        k.go("Lose", this.score);
+      }
     });
   }
 
