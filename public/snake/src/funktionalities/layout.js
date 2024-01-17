3d2439f9237585e0d98d21f4d66e59f5b5f82a4c
elisabethedert <elisabeth.edert@gmail.com>
2024-01-17 18:01:58 +0100

    addDescription and endText functions

--- public/snake/src/funktionalities/layout.js
+++ public/snake/src/funktionalities/layout.js
@@ -6,7 +6,7 @@ export default class Layout {
 
   addButton(rectX, rectY, txt, posX, posY, scene) {
     k.loadSound("buttonclick", "sound/button.mp3");
-    k.loadFont("lilitaone", "font/LilitaOne-Regular.ttf")
+    k.loadFont("lilitaone", "font/LilitaOne-Regular.ttf");
     let text = add([
       k.rect(rectX, rectY),
       k.pos(posX, posY),
@@ -16,7 +16,7 @@ export default class Layout {
     ]);
 
     let button = add([
-      k.text(txt, {font:"lilitaone"}),
+      k.text(txt, { font: "lilitaone" }),
       k.pos(posX, posY),
       k.anchor("center"),
       k.area({ cursor: "pointer" }),
@@ -44,16 +44,54 @@ export default class Layout {
   }
 
   addText(txt, posX, posY) {
-    k.loadFont("lilitaone", "font/LilitaOne-Regular.ttf")
+    k.loadFont("lilitaone", "font/LilitaOne-Regular.ttf");
     k.add([
-      k.text(
-        txt, {font:"lilitaone"}
-      ),
+      k.text(txt, { font: "lilitaone" }),
       k.pos(posX / 2, posY / 2),
       k.color(249, 121, 25),
       k.scale(1.5),
       k.anchor("center"),
     ]);
+  }
+
+  addDescription(txt, posX, posY) {
+    k.loadFont("lilitaone", "font/LilitaOne-Regular.ttf");
+    k.add([
+      k.text(txt, { font: "lilitaone" }),
+      k.pos(posX / 2, posY / 2),
+      k.color(168, 100, 2),
+      k.scale(0.6),
+      k.anchor("center"),
+    ]);
+  }
+
+  addEndText(score) {
+    let text;
+    if (score == 0 || score > 1) {
+      if (score == 0) {
+        text = "Denk dran Früchte zu fressen um Punkte zu erhalten";
+      } else if (score != 1 && score <= 5) {
+        text = "Schon ganz okay!";
+      } else if (score <= 10) {
+        text = "Super Punktzahl!";
+      } else if (score >= 11) {
+        text = "Wahnsinn, sehr gut gespielt!";
+      }
+      this.addText(
+        "Du hast " + score + " Früchte gefressen",
+        Config.width,
+        380
+      );
+      this.addDescription(text, Config.width, 660);
+    } else if (score == 1) {
+      text = "Nächstes mal wird sicher besser!";
+      this.addText(
+        "Du hast " + score + " Frucht gefressen",
+        Config.width, 
+        380
+      );
+      this.addDescription(text, Config.width, 660);
+    }
   }
 
   addImage(image) {
--- public/snake/src/scenes/start.js
+++ public/snake/src/scenes/start.js
@@ -18,26 +18,39 @@ export default function Start(score) {
 
   if (score == null) {
     // show start scene with welcome-text
-    layout.addText("Willkommen zu Snake", Config.width, 480);
-    layout.addButton(220, 70, "Spielen", 245, 400, "Game");
-    layout.addButton(220, 70, "Steuerung", 605, 400, "Navigation");
-    
+    layout.addText("Willkommen zu Snake", Config.width, 380);
+    layout.addDescription(
+      "Versuche so viele Früchte wie möglich zu fressen",
+      Config.width,
+      520
+    );
+    layout.addDescription(
+      "aber pass vor den Hindernissen auf,",
+      Config.width,
+      580
+    );
+    layout.addDescription(
+      "sobald du mit ihnen kollidierst ist das Spiel vorbei.",
+      Config.width,
+      640
+    );
+    layout.addDescription(
+      "Du verlierst Punkte, wenn du die Snake über sich selbst leitest.",
+      Config.width,
+      700
+    );
+    layout.addDescription(
+      "Frisst du mehrere Früchte, bist du vor Hindernissen und Punkverlust geschützt.",
+      Config.width,
+      760
+    );
+    layout.addButton(220, 70, "Spielen", 245, 480, "Game");
+    layout.addButton(220, 70, "Steuerung", 605, 480, "Navigation");
   } else {
     // show start scene with score
-    if (score == 1) {
-      layout.addText(
-        "Du hast " + score + " Frucht gefressen",
-        Config.width,
-        480
-      );
-    } else {
-      layout.addText(
-        "Du hast " + score + " Früchte gefressen",
-        Config.width,
-        480
-      );
-    }
-    layout.addButton(220, 70, "Nochmal?", 245, 400,"Game");
-    layout.addButton(220, 70, "Steuerung", 605, 400, "Navigation");
+    layout.addEndText(score)
+   
+    layout.addButton(220, 70, "Nochmal?", 245, 480, "Game");
+    layout.addButton(220, 70, "Steuerung", 605, 480, "Navigation");
   }
 }
